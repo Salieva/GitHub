0f6c30ebf6718f6bbf11d8c2ee3427e6e224bebb
Salieva <okilasalieva@gmail.com>
2018-03-18 09:43:43 -0700

    задания 7, 8
    сценарии по разделам админки и стикерам

--- okila-selenium/test-google.py
+++ okila-selenium/test-google.py
@@ -3,17 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
+    #wd = webdriver.Chrome()
+    #wd = webdriver.Firefox(capabilities={"marionette": False})
+    wd = webdriver.Firefox(firefox_binary="c:\\Users\\Bekhzod\\AppData\\Local\\Firefox Nightly\\firefox.exe")
+    #wd = webdriver.Firefox()
+    #wd = webdriver.Edge()
+    #wd = webdriver.Ie()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
-
 
 def test_example(driver):
     driver.get("http://www.google.com/")
     driver.find_element_by_name("q").send_keys("webdriver\n")
     #driver.find_element_by_name("btnG").click()
     WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google Search"))
+    #driver.quit()
--- okila-selenium/test-litecart.py
+++ okila-selenium/test-litecart.py
@@ -7,7 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
+    #wd = webdriver.Chrome()
+    #wd = webdriver.Ie()
+    #wd = webdriver.Firefox(firefox_binary="c:\\program files\\Firefox Nightly\\firefox.exe")
+    #wd = webdriver.Firefox(capabilities={"marionette": False})
+    #wd = webdriver.Firefox(firefox_binary="c:\\program files\\Mozilla Firefox\\firefox.exe")
+    #wd = webdriver.Edge()
     print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
@@ -15,8 +20,10 @@ def driver(request):
 
 def test_example(driver):
     driver.get("http://localhost/litecart/admin/")
+    driver.delete_all_cookies()
+    driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
-    driver.find_element_by_name("remember_me").click()
+    #driver.find_element_by_name("remember_me").click()
     driver.find_element_by_name("login").click()
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "sidebar")))
