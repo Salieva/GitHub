3964c820484594e8d0c57923fb0d1ebd347fdc3f
Salieva <okilasalieva@gmail.com>
2018-04-14 13:07:34 -0700

    задание 4
    выполнено

--- okila-selenium/test_python_task_4.py
+++ okila-selenium/test_python_task_4.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
 import pytest
 from group import Group
 from application import Application
 
-
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
 @pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-
 
 
 def test_python_lesson_1_task_1(app):
@@ -30,6 +19,7 @@ def test_python_lesson_1_task_1(app):
     app.return_to_group_page()
     app.logout()
 
+
 def test_python_lesson_empty_1_task_1(app):
     app.open_home_page()
     app.login(username="admin", password="secret")
@@ -38,5 +28,3 @@ def test_python_lesson_empty_1_task_1(app):
     app.return_to_group_page()
     app.logout()
 
-if __name__ == '__main__':
-    unittest.main()
